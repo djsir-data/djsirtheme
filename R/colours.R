7d2c6cce33d5b84e93b10452ee5464ed21ae6250
Annett Finger <annett.finger@ecodev.vic.gov.au>
2023-02-09 16:37:57 +1100

    djsir colour definitions

--- R/colours.R
+++ R/colours.R
@@ -1,14 +1,57 @@
 
 
-#' Generate Red
+#' DJSIR colours
 #'
-#' @param n integer value
+#' A set of colours and their # values, as defined by the DJSIR colour scheme
 #'
-#' @return character for colour
-#' @export
+#' @name djsir_colours
 #'
 #' @examples
-#' colours(1)
-colours <- function(n){
-  return('red')
-}
+#' colours()
+
+NULL
+
+#' @rdname djsir_colours
+#' @export
+djpr_cobalt <- "#004C97"
+
+#' @rdname djsir_colours
+#' @export
+djpr_bondi_blue <- "#009CA6"
+
+#' @rdname djsir_colours
+#' @export
+djpr_lima <- "#78BE20"
+
+#' @rdname djsir_colours
+#' @export
+djpr_electric_lime <- "#CEDC00"
+
+#' @rdname djsir_colours
+#' @export
+djpr_royal_blue <- "#13345E"
+
+#' @rdname djsir_colours
+#' @export
+djpr_green <- "#69972C"
+
+#' @rdname djsir_colours
+#' @export
+djpr_persimmon <- "#E35205"
+
+#' @rdname djsir_colours
+#' @export
+djpr_dark_tangerine <- "#FF9E1B"
+
+#' @rdname djsir_colours
+#' @export
+djpr_black_rock <- "#343641"
+
+#' @rdname djsir_colours
+#' @export
+djpr_cool_grey_11 <- "#53565A"
+
+#' @rdname djsir_colours
+#' @export
+djpr_cool_grey_1 <- "#D9D9D6"
+
