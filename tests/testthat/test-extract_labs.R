0db2a1cd043931544c2b4786175cc528bb6c468f
Annett Finger <annett.finger@ecodev.vic.gov.au>
2023-02-28 11:57:52 +1100

    Added two more test scripts: extract_labs and get_plot_data. Both working fine after some fixing.

---
+++ tests/testthat/test-extract_labs.R
@@ -0,0 +1,52 @@
+library(ggplot2)
+library(patchwork)
+
+plot1 <- ggplot(economics, aes(x = date, y = unemploy)) +
+  geom_point() +
+  theme_djsir() +
+  labs(subtitle = "Plot 1 subtitle",
+       caption = "Plot 1 caption",
+       title = "Plot 1 title")
+
+# Create a second plot
+plot2 <- ggplot(economics, aes(x = date, y = uempmed)) +
+  geom_point() +
+  theme_djsir() +
+  labs(subtitle = "Plot 2 subtitle")
+
+# Now combine these plots using patchwork
+comb_plots <- plot1 + plot2 +
+  plot_annotation(title = "Combined plot title",
+                  caption = "Data source")
+
+test_that("extract_labs() behaves as expected", {
+
+  # Single plot
+  expect_identical(extract_labs(plot1, "subtitle"), "Plot 1 subtitle")
+  expect_identical(extract_labs(plot1, "caption"), "Plot 1 caption")
+  expect_identical(extract_labs(plot1, "title"), "Plot 1 title")
+  expect_error(extract_labs(plot1, "foobar"))
+
+
+  expect_identical(extract_labs(comb_plots, "title"), "Combined plot title")
+  expect_identical(extract_labs(comb_plots, "caption"), "Data source")
+
+  expect_error(extract_labs(LETTERS, "title"))
+})
+
+test_that("remove_labs() behaves as expected", {
+  expect_null(remove_labs(plot1)$labels$title)
+  expect_null(remove_labs(plot1)$labels$subtitle)
+  expect_null(remove_labs(plot1)$labels$caption)
+
+  expect_null(remove_labs(comb_plots)$patches$annotation$title)
+  expect_null(remove_labs(comb_plots)$patches$annotation$subtitle)
+  expect_null(remove_labs(comb_plots)$patches$annotation$caption)
+
+  vdiffr::expect_doppelganger("regular plot, no labels",
+                              remove_labs(plot1))
+
+  vdiffr::expect_doppelganger("pathwork plot, no labels",
+                              remove_labs(comb_plots))
+})
+
---
+++ tests/testthat/test-get_plot_data.R
@@ -0,0 +1,156 @@
+library(dplyr)
+library(ggplot2)
+
+test_that("get_plot_data() returns plot data passed to ggplot() call", {
+    test_plot <-
+      ggplot(iris, aes(Petal.Length, Petal.Width)) +
+      geom_point()
+
+    expect_equal(
+      get_plot_data(test_plot),
+      iris[, c("Petal.Length", "Petal.Width")]
+    )
+  })
+
+test_that("get_plot_data() defaults to the last plot", {
+    qplot(Sepal.Width, Sepal.Length, data=iris)
+
+  expect_equal(
+      get_plot_data(),
+      iris[, c("Sepal.Width", "Sepal.Length")]
+    )
+  })
+
+test_that("get_plot_data() supports formulae in aesthetics", {
+    test_plot <-
+      ggplot(
+        iris,
+        aes(
+          Sepal.Width - mean(Sepal.Width),
+          scale(Sepal.Length, center = T, scale = T),
+          colour=Species
+        )
+      ) +
+      geom_point()
+
+    expect_equal(
+      get_plot_data(test_plot),
+      with(iris, data.frame(
+        Sepal.Width - mean(Sepal.Width),
+        scale(Sepal.Length, center = T, scale = T),
+        Species,
+        check.names=F
+      ))
+    )
+  })
+
+test_that("get_plot_data() detects aesthetics across multiple layers", {
+    test_plot <-
+      ggplot(iris, aes(Petal.Length, Petal.Width)) +
+      # Adds Sepal.Length to expected return list
+      geom_point(aes(size=Sepal.Length), colour="blue") +
+      # Adds Sepal.Width to expected return list; Sepal.Length already on list
+      # and should not be returned twice
+      geom_point(aes(alpha=Sepal.Width, y=Sepal.Length), colour="red")
+
+    expect_equal(
+      get_plot_data(test_plot),
+      iris[, c("Petal.Length", "Petal.Width", "Sepal.Length", "Sepal.Width")]
+    )
+  })
+
+test_that("get_plot_data() ignores layers that have their own data", {
+    test_dataset <-
+      group_by(iris, Species) %>%
+      summarise_all(mean)
+    test_plot <-
+      ggplot(iris, aes(Petal.Length, Petal.Width)) +
+      # Adds Sepal.Length to expected return list
+      geom_point(aes(size=Sepal.Length), colour="blue") +
+      # Should be ignored, not using plot data
+      geom_point(data=test_dataset, aes(colour=Species))
+
+    expect_equal(
+      get_plot_data(test_plot),
+      iris[, c("Petal.Length", "Petal.Width", "Sepal.Length")]
+    )
+  })
+
+test_that("get_plot_data() preserves row names", {
+    qplot(mpg, hp, data=mtcars) +
+      geom_point()
+
+    expect_equal(
+      get_plot_data(),
+      mtcars[, c("mpg", "hp")]
+    )
+
+    expect_equal(
+      rownames(get_plot_data()),
+      rownames(mtcars)
+    )
+  })
+
+test_that("get_plot_data() exports facet variables", {
+    test_plot <- ggplot(iris, aes(Petal.Width, Petal.Length)) +
+      facet_wrap(~Species) +
+      geom_point()
+
+    expect_equal(
+      get_plot_data(test_plot),
+      iris[, c("Petal.Width", "Petal.Length", "Species")]
+    )
+
+    test_plot <- ggplot(mtcars) +
+      facet_grid(cyl ~ gear) +
+      geom_point(aes(mpg, hp))
+    expect_equal(
+      get_plot_data(test_plot),
+      mtcars[, c("mpg", "hp", "cyl", "gear")]
+    )
+
+    # Expressions in facets
+    test_plot <- ggplot(mtcars, aes(mpg, hp)) +
+      facet_grid((cyl + carb) ~ paste("g", gear, sep=":")) +
+      geom_point()
+
+    expect_equal(
+      get_plot_data(test_plot),
+      with(mtcars, data.frame(
+        mpg,
+        hp,
+        (cyl + carb),
+        paste("g", gear, sep=":"),
+        check.names=F,
+        row.names=row.names(mtcars)
+      ))
+    )
+  })
+
+test_that("get_plot_data() exports patchwork plot data", {
+    p1 <- ggplot(mtcars, aes(x = wt, y = mpg)) +
+      geom_point() +
+      labs(subtitle = "Plot 1 subtitle",
+           title = "Plot 1 title")
+
+    p2 <- ggplot(economics, aes(x = date, y = unemploy)) +
+      geom_line() +
+      labs(subtitle = "Plot 2 subtitle",
+           title = "Plot 2 title",
+           caption = "Plot 2 caption")
+
+    comb <- patchwork::wrap_plots(p1, p2) +
+      patchwork::plot_annotation(title = "Combined plot title",
+                                 subtitle = "Combined plot subtitle",
+                                 caption = "Combined plot caption")
+
+    df1 <- mtcars %>%
+      dplyr::select(wt, mpg)
+
+    df2 <- ggplot2::economics %>%
+      dplyr::select(date, unemploy)
+
+    expect_equal(dplyr::bind_rows(df1, df2),
+                 get_plot_data(comb))
+  })
+
